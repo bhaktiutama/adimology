7dc8ded2f5c4315f9c2793359bce4de340f417a2
Yogi Prasetya <yogiprsetya@gmail.com>
2026-02-20 09:53:46 +0700

    add eslint prettier husky

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// eslint.config.mjs
+import { defineConfig } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    extends: [
+      nextVitals,
+      nextTs,
+      prettier,
+    ],
+    rules: {
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+]);
